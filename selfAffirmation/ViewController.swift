1afdc2291ae21c6cabdf835e438a399023682988
Belle Zou <bellezou101@yahoo.com>
2020-07-16 11:49:32 -0400

    This is the affirmations page.

--- selfAffirmation/ViewController.swift
+++ selfAffirmation/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var imageView: UIImageView!
+    //var images = [self.imageView, self.imageViewTwo, self.imageViewThree]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+                  }
+       
+    override func viewWillAppear(_ animated: Bool) {
+        imageView.isHidden = false
+        
+    }
+   
+
+       var count = 1
+   
+    @IBAction func buttonTapped(_ sender: Any) {
+        imageView.isHidden = false
+        //var imageName = affirmations[Int.random(in:0...2)]
+        imageView.image = UIImage(named:affirmations[count%19])
+        count = count+1
+    }
+    
+
+       
+    
+var affirmations = ["imageOne.png", "imageTwo.png", "imageThree.png", "ImageFour.png", "ImageFive.png", "ImageSix.png", "ImageSeven.png", "ImageEight.png", "ImageNine.png", "ImageTen.png", "ImageEleven.png", "ImageTwelve.png", "ImageThirteen.png", "ImageFourteen.png", "ImageFifteen.png", "ImageSixteen.png", "ImageSeventeen.png", "ImageEighteen.png", "ImageNineteen.png"]
+    
+func pickAffirmation() -> String {
+    let random = Int.random(in: 1..<20)
+    let affirmation = affirmations[random] // note about how this work below!
+    return affirmation
+    
+
+let randomAffirmation = pickAffirmation()
+print(randomAffirmation)
 
 }
+}
+
 
